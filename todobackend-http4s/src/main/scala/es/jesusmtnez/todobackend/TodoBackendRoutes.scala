bffa9d1f1dd3e57dbbda921586a20959fb0d3bb4
JesusMtnez <jesusmartinez93@gmail.com>
2023-08-31 10:02:52 +0200

    feat(http4s): setup TodoBackendRoutes to hold all defined routes

--- todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendRoutes.scala
+++ todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendRoutes.scala
@@ -5,10 +5,16 @@ import cats.effect.Sync
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 
-object About:
-  def routes[F[_]: Sync](): HttpRoutes[F] =
+object TodoBackendRoutes:
+
+  def about[F[_]: Sync](): HttpRoutes[F] =
     val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F]:
       case GET -> Root / "about" =>
         Ok(BuildInfo.toString)
+
+  def todo[F[_]: Sync](): HttpRoutes[F] =
+    // val dsl = new Http4sDsl[F] {}
+    // import dsl._
+    HttpRoutes.empty
--- todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendServer.scala
+++ todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendServer.scala
@@ -1,9 +1,11 @@
 package es.jesusmtnez.todobackend
 
 import cats.effect.*
+import cats.implicits.*
 import fs2.io.net.Network
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.middleware.Logger
+import org.http4s.implicits.*
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.slf4j.Slf4jFactory
 
@@ -11,7 +13,13 @@ object TodoBackendServer {
 
   def run[F[_]: Async: Network]: F[Nothing] =
     implicit val loggerFactory: LoggerFactory[F] = Slf4jFactory.create[F]
-    val httpApp = Logger.httpApp(true, true)(About.routes[F]().orNotFound)
+
+    val routes =
+      TodoBackendRoutes.about[F]()
+        <+> TodoBackendRoutes.todo[F]()
+
+    val httpApp = Logger.httpApp(true, true)(routes.orNotFound)
+
     EmberServerBuilder.default[F].withHttpApp(httpApp).build.useForever
 
 }
