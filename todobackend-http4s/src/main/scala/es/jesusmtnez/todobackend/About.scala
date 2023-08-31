778cc2ee7918bdc098399991119a6b4a1d9a299e
JesusMtnez <jesusmartinez93@gmail.com>
2023-08-31 09:32:24 +0200

    fix(http4s): format About.scala

--- todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/About.scala
+++ todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/About.scala
@@ -7,7 +7,7 @@ import org.http4s.dsl.Http4sDsl
 
 object About:
   def routes[F[_]: Sync](): HttpRoutes[F] =
-    val dsl = new Http4sDsl[F]{}
+    val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F]:
       case GET -> Root / "about" =>
