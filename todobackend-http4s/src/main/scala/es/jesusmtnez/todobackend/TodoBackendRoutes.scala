f8d127af93639fd42db8e88dcfef22895b4c0131
JesusMtnez <jesusmartinez93@gmail.com>
2023-09-02 07:09:58 +0200

    feat(http4s): add GET /

--- todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendRoutes.scala
+++ todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendRoutes.scala
@@ -1,8 +1,11 @@
 package es.jesusmtnez.todobackend
 
 import buildinfo.BuildInfo
-import cats.effect.Sync
+import cats.effect.*
+import cats.syntax.all.*
+import es.jesusmtnez.todobackend.domain.*
 import org.http4s.HttpRoutes
+import org.http4s.circe.*
 import org.http4s.dsl.Http4sDsl
 
 object TodoBackendRoutes:
@@ -14,7 +17,15 @@ object TodoBackendRoutes:
       case GET -> Root / "about" =>
         Ok(BuildInfo.toString)
 
-  def todo[F[_]: Sync](): HttpRoutes[F] =
-    // val dsl = new Http4sDsl[F] {}
-    // import dsl._
-    HttpRoutes.empty
+  def todo[F[_]: Concurrent](repository: TodoRepository[F]): HttpRoutes[F] =
+    val dsl = new Http4sDsl[F] {}
+    import dsl._
+
+    import org.http4s.circe.CirceEntityCodec.*
+
+    HttpRoutes.of[F]:
+      case GET -> Root =>
+        for {
+          items <- repository.getAll()
+          response <- Ok(items)
+        } yield response
--- todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendServer.scala
+++ todobackend-http4s/src/main/scala/es/jesusmtnez/todobackend/TodoBackendServer.scala
@@ -4,22 +4,38 @@ import cats.effect.*
 import cats.implicits.*
 import fs2.io.net.Network
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.middleware.Logger
+import org.http4s.server.middleware.*
 import org.http4s.implicits.*
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.slf4j.Slf4jFactory
+import org.http4s.Method
+import org.typelevel.ci.CIString
 
 object TodoBackendServer {
 
-  def run[F[_]: Async: Network]: F[Nothing] =
+  def run[F[_]: Async: Network]: F[Unit] =
     implicit val loggerFactory: LoggerFactory[F] = Slf4jFactory.create[F]
 
-    val routes =
-      TodoBackendRoutes.about[F]()
-        <+> TodoBackendRoutes.todo[F]()
+    val cors =
+      CORS.policy.withAllowOriginAll
+        .withAllowMethodsIn(
+          Set(
+            Method.GET,
+            Method.POST,
+            Method.DELETE,
+            Method.OPTIONS,
+            Method.PATCH
+          )
+        )
+        .withAllowHeadersIn(Set(CIString("content-type")))
 
-    val httpApp = Logger.httpApp(true, true)(routes.orNotFound)
+    for {
+      repository <- TodoRepositoryImpl.inMemory[F]()
+      routes = TodoBackendRoutes.about[F]()
+        <+> TodoBackendRoutes.todo[F](repository)
 
-    EmberServerBuilder.default[F].withHttpApp(httpApp).build.useForever
+      httpApp <- cors(Logger.httpApp(true, true)(routes.orNotFound))
+      _ <- EmberServerBuilder.default[F].withHttpApp(httpApp).build.useForever
+    } yield ()
 
 }
